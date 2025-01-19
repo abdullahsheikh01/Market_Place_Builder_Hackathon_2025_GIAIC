bfd4f123b0dca5e4ec322824e35a9885e280ebed
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2025-01-20 00:18:15 +0500

    Make dynamic prop type right

--- my-app/src/app/api/fetch/categoryItem/[id]/route.ts
+++ my-app/src/app/api/fetch/categoryItem/[id]/route.ts
@@ -1,12 +1,11 @@
 import { client } from "@/sanity/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 export async function GET(req:NextRequest,content:{
-    params:{
-        id:string
-    }
-}){
+    params:Promise<{id:string}>}
+){
     try{
-        const query = `*[_type=="product"&&category._ref=="${content.params.id}"]`;
+      const id = await(content.params);
+        const query = `*[_type=="product"&&category._ref=="${id.id}"]`;
         const resp = await client.fetch(query);
         return NextResponse.json(resp);
     }
